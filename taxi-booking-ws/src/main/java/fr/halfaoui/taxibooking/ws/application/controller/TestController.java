1a59936edf0a5b1855208acfe00757c5bc8d4b10
Amirouche Halfaoui <amirouche.halfaoui@gmail.com>
2016-02-14 23:55:40 +0100

    OAuth2 server annotations configuration

--- taxi-booking-ws/src/main/java/fr/halfaoui/taxibooking/ws/application/controller/TestController.java
+++ taxi-booking-ws/src/main/java/fr/halfaoui/taxibooking/ws/application/controller/TestController.java
@@ -1,6 +1,5 @@
 package fr.halfaoui.taxibooking.ws.application.controller;
 
-import org.springframework.context.annotation.Role;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @RequestMapping(value = "/test", method = GET)
-    
+    @PreAuthorize("#oauth2.clientHasRole('ROLE_TRUSTED_CLIENT') or hasRole('ROLE_USER')")
     public String test() {
         return "Hello World";
     }
